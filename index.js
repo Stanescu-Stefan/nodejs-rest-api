8b7153ac547a6c8c6bab9bb7bcad8708866c99d6
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2022-03-22 13:15:01 +0200

    setting up test endpoint

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.use("/test", (req, res) => {
+  console.log("Received request");
+});
+
 app.listen(3000, () => {
   console.log("I am ready to listen to you");
 });
